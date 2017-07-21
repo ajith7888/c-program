185dc7033086328520cdc08aa0c8fae43bcc9076
ajith7888 <30342594+ajith7888@users.noreply.github.com>
2017-07-21 16:59:44 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("ENTER THE NUMBER:\n");
+scanf("%d",&n);
+if(n<=0)
+{
+if(n==o)
+printf("zero\n");
+else
+printf("negative number\n");
+}
+else
+{
+printf("positive number\n");
+}
+getch();
+return(0);
+}
